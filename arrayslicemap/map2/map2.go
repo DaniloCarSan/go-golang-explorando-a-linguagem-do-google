7abebc5db0b26833178ee901957451506fdb9372
Danilo Santos <danilocarsan@gmail.com>
2022-08-12 20:59:41 -0300

    iniciando map na declaração

---
+++ arrayslicemap/map2/map2.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	salarios := map[string]float64{
+		"Danilo": 5698.58,
+		"Lorem":  8947.89,
+		"Mario":  6800.90,
+	}
+
+	fmt.Println(salarios)
+
+	salarios["Juliano"] = 3500.58
+
+	fmt.Println(salarios)
+
+	for nome, salario := range salarios {
+		fmt.Println(nome, salario)
+	}
+
+}
