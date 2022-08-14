a0fbe308e94aa535773f03870963d832f8cae313
Danilo Santos <danilocarsan@gmail.com>
2022-08-14 12:34:16 -0300

    criando e utilizando pacote local

---
+++ pacote/reuso/go.mod
@@ -0,0 +1,7 @@
+module github.com/DaniloCarSan/curso-golang/pacote/reuso
+
+go 1.19
+
+replace github.com/DaniloCarSan/area => ../../../area
+
+require github.com/DaniloCarSan/area v0.0.0-00010101000000-000000000000
---
+++ pacote/reuso/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/DaniloCarSan/area"
+)
+
+func main() {
+	fmt.Println(area.Circ(3))
+	fmt.Println(area.Rect(5.0, 2.9))
+}
+
+// go run main.go
+// go init mod
+// go mod tidy
