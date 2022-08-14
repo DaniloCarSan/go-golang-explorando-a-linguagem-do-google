025e523b327fa2655ce4a5feaf90b93af2708908
Danilo Santos <danilocarsan@gmail.com>
2022-08-14 13:03:17 -0300

    usando pacote remoto

---
+++ pacote/usandolib/go.mod
@@ -0,0 +1,5 @@
+module github.com/DaniloCarSan/curso-golang/pacote/usandolib
+
+go 1.19
+
+require github.com/DaniloCarSan/goarea v0.0.0-20220814153830-7e5820923292
---
+++ pacote/usandolib/usandolib.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/DaniloCarSan/goarea"
+)
+
+func main() {
+	fmt.Println(goarea.Circ(10))
+}
+
+//go run usandolib.go
+//go mod tidy
