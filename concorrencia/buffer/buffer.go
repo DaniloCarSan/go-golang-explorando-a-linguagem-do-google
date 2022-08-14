885d334e92788f3642b9ab7f34cce26e502e3743
Danilo Santos <danilocarsan@gmail.com>
2022-08-14 16:03:02 -0300

    testando channel com buffer

---
+++ concorrencia/buffer/buffer.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func rotina(ch chan int) {
+
+	ch <- 1
+	ch <- 2
+	ch <- 3
+	ch <- 4
+	fmt.Println("Enviou !")
+	ch <- 5
+	ch <- 6
+
+}
+
+func main() {
+
+	ch := make(chan int, 3)
+
+	go rotina(ch)
+
+	time.Sleep(time.Second)
+	fmt.Println("Chegou aqui:", <-ch)
+
+}
